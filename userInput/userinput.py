a330a021c5f369dfece857c9d039c1208258fc9c
Muhamad ZIbran Fitadiyatama <katyushachan191202@gmail.com>
2020-09-19 16:34:16 +0700

    Add files via upload

---
+++ userInput/userinput.py
@@ -0,0 +1,9 @@
+nama = input("Masukkan nama anda :")
+umur = input("Masukkan umur anda :")
+alamat = input("Masukkan alamat anda :")
+telepon = input("Masukkan nomor telepon anda :")
+
+print("nama anda adalah " + nama)
+print("umur anda adalah " + umur + " tahun")
+print("alamat anda " + alamat)
+print("nomor telepon anda adalah " + telepon)
